cd62d84563dda448aa56c2491ca950337d70225e
Arun <56299946+ArunReaper@users.noreply.github.com>
2026-06-14 00:43:27 +0530

    Cors config

--- booktheturf_backend/src/main/java/com/dev/booktheturf/config/WebConfig.java
+++ booktheturf_backend/src/main/java/com/dev/booktheturf/config/WebConfig.java
@@ -10,9 +10,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+        registry.addMapping("/**")
+                .allowedOriginPatterns(
+                        "http://localhost:*",
+                        "https://*.vercel.app"
+                )
+                .allowedMethods("*")
                 .allowedHeaders("*");
     }
 }
