dcceb4efc8ac204aff43dac3880e12b0a4f31ab0
Arun <56299946+ArunReaper@users.noreply.github.com>
2026-06-14 00:40:04 +0530

    Shift to Vercel

--- booktheturf_frontend/src/api/turfApi.ts
+++ booktheturf_frontend/src/api/turfApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const turfApi = axios.create({
-    baseURL: "http://localhost:8080/api"
+    baseURL: "https://book-the-turf-production.up.railway.app/api/"
 });
 
 export default turfApi;
